3bb45e3268593befef4aa26396bd3837bd6f29b1
wafasdbz <waf_saidane@yahoo.fr>
2022-10-25 17:31:49 +0200

    security v0

---
+++ src/main/java/com/example/gestionMed/securityConfig/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.example.gestionMed.securityConfig;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Role;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.gestionMed.repository.UtilisateurRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService{
+	@Autowired
+	private UtilisateurRepository utilisateurRepository;
+	@Transactional(readOnly = true)
+	public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+		Account account = accountRepository.findByLogin(login);
+		if ( account == null )
+			throw new UsernameNotFoundException("Invalides login/mot de passe");
+		Set grantedAuthorities = new HashSet<>();
+		for (Role role : account.getRoles()){ 
+			grantedAuthorities.add(new SimpleGrantedAuthority(role.getLibelle())); }
+		return new User(account.getLogin(), account.getPassword(), grantedAuthorities); } }
---
+++ src/main/java/com/example/gestionMed/securityConfig/WebSecurityConfiguration.java
@@ -0,0 +1,47 @@
+package com.example.gestionMed.securityConfig;
+
+
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		
+		http
+		 .csrf().disable()
+		 .authorizeRequests() // ACLs
+		 .antMatchers(HttpMethod.GET,"/items/**").permitAll()
+		 .antMatchers("/emprunt/**").authenticated()
+		 .anyRequest().authenticated()
+		 .and()
+		 .formLogin()// Page de login
+		 .permitAll()
+		 .and()
+		 .logout()
+		 .logoutUrl("/logout")
+		.logoutSuccessUrl("/api/utilisateurs")
+		 .invalidateHttpSession(true);
+		 
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		
+		// Ici on utilise un realm mémoire, AuthenticationManagerBuilder permet
+		// également de facilement de se connecter à un annuaire LDAP ou une bd
+		
+		 auth.inMemoryAuthentication().withUser("user").password("{noop}"+ "password")
+		 .roles("USER")
+		 .and().withUser("admin").password("{noop}"+ "password")
+		 .roles("USER", "ADMIN");
+	}
+}
